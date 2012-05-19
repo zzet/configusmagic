e34f2e639ebc7ef381020855e5b44c181b0f0525
Andrey Subbota <subbota@gmail.com>
2012-05-19 15:23:59 +0400

    Hash builder complete! Yarrr

--- lib/configusmagic.rb
+++ lib/configusmagic.rb
@@ -4,8 +4,9 @@ module Configusmagic
   autoload :Builder, 'configusmagic/builder'
   autoload :RecursiveBuilder, 'configusmagic/recursive_builder'
 
-  def self.build(&block)
+  def self.build(environment, &block)
     config = Configusmagic::Builder.new block
     config.build
+    config.get environment
   end
 end
